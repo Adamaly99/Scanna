ac9846f2100444567cca3ddfdaed72aea055007b
Adamaly99 <adamsly990@gmail.com>
2026-09-12 23:42:49 +0000

    Create src/hooks/index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Hooks - Logique réutilisable dans les composants
+ */
+
+export { useSubscription, usePremiumGate, useSubscriptionLimits } from './useSubscription';
+export { usePremium } from './useSubscriptionGate';
