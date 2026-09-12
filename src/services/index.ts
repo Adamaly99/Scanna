d3e8d1876a5e5aa4b789cb5ee597e61fe0c30353
Adamaly99 <adamsly990@gmail.com>
2026-09-12 23:41:02 +0000

    Create src/services/index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Services - Couche métier centralisée
+ */
+
+export { logger } from './logger';
+export { fileStorage } from './fileStorage';
+export { database } from './database';
+
+export type { LogEntry } from './logger';
+export type { FileStorageInfo } from './fileStorage';
