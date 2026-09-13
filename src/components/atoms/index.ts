bedc2d282f61f9cadb9f2b77e160ad42ab8fc28d
Adamaly99 <adamsly990@gmail.com>
2026-09-13 15:57:09 +0000

    Create src/components/atoms/index.ts

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { Text } from './Text';
+export { Loader } from './Loader';
+export { Card } from './Card';
+export { Icon } from './Icon';
