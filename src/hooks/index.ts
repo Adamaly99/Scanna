9745fc8e78caaa545ac459c86aedc010c3b55d51
Adamaly99 <adamsly990@gmail.com>
2026-09-13 15:54:17 +0000

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
-/**
- * Hooks - Logique réutilisable dans les composants
- */
-
 export { useSubscription, usePremiumGate, useSubscriptionLimits } from './useSubscription';
 export { usePremium } from './useSubscriptionGate';
+export { useCamera } from './useCamera';
+export { useScanner } from './useScanner';
+export { useDocuments } from './useDocuments';
