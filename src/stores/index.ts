7fee67307c2d70c1e8c6ba63f3a5f0ac62165cc4
Adamaly99 <adamsly990@gmail.com>
2026-09-13 15:52:10 +0000

    Create src/stores/index.ts

---
+++ src/stores/index.ts
@@ -0,0 +1,6 @@
+export { useAppStore } from './appStore';
+export type { AppTheme, AppLocale } from './appStore';
+
+export { useScanStore } from './scanStore';
+
+export { useDocumentStore } from './documentStore';
